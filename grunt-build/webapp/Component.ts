814395d2ddf4075cb731efa14d9757f174cef333
chaofengdang <chaofengdang@gmai.com>
2021-03-05 11:00:38 +0800

    modify .gitignore

--- grunt-build/webapp/Component.ts
+++ grunt-build/webapp/Component.ts
@@ -1,50 +1,40 @@
-sap.ui.define(
-    [
-        'sap/ui/core/UIComponent',
-        'sap/ui/Device',
-        'manage-ui/model/models',
-        'sap/ui/model/json/JSONModel',
-        'manage-ui/javascript/utils',
-        'sap/base/util/uid'
-    ],
-    function (UIComponent, Device, models, JSONModel, utils, uid) {
-        'use strict';
+sap.ui.define(['sap/ui/core/UIComponent'], function (UIComponent) {
+    'use strict';
 
-        class Component extends UIComponent {
-            constructor() {
-                const fnClass = UIComponent.extend('manage-ui.Component', {
-                    metadata: {
-                        manifest: 'json',
-                        config: {
-                            fullwidth: true
-                        }
+    class Component extends UIComponent {
+        constructor() {
+            const fnClass = UIComponent.extend('manage-ui.Component', {
+                metadata: {
+                    manifest: 'json',
+                    config: {
+                        fullwidth: true
                     }
-                });
-                Component.prototype.getMetadata = fnClass.prototype.getMetadata;
+                }
+            });
+            Component.prototype.getMetadata = fnClass.prototype.getMetadata;
 
-                super('manage-ui.Component');
-            }
-
-            /**
-             * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
-             * @public
-             * @override
-             */
-            async init(args) {
-                // call the base component's init function
-                UIComponent.prototype.init.apply(this, args);
-            }
-
-            /**
-             * @author      Adam Dang
-             * @description before destroy
-             */
-            destroy() {
-                // call the base component's destroy function
-                UIComponent.prototype.destroy.apply(this, arguments);
-            }
+            super('manage-ui.Component');
         }
 
-        return Component;
+        /**
+         * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
+         * @public
+         * @override
+         */
+        async init(args) {
+            // call the base component's init function
+            UIComponent.prototype.init.apply(this, args);
+        }
+
+        /**
+         * @author      Adam Dang
+         * @description before destroy
+         */
+        destroy() {
+            // call the base component's destroy function
+            UIComponent.prototype.destroy.apply(this, arguments);
+        }
     }
-);
+
+    return Component;
+});
