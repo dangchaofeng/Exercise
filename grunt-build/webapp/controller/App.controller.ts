0882f9c69571e9844ea9d9c80a2ca9269d55d233
chaofengdang <chaofengdang@gmai.com>
2021-03-05 10:12:16 +0800

    update grunt-build

--- grunt-build/webapp/controller/App.controller.ts
+++ grunt-build/webapp/controller/App.controller.ts
@@ -1,19 +1,11 @@
-/*
- * @Author: your name
- * @Date: 2020-10-20 14:56:00
- * @LastEditTime: 2020-10-26 16:35:44
- * @LastEditors: your name
- * @Description: In User Settings Edit
- * @FilePath: \com.sap.cf.sales.self.billing.general\webapp\controller\App.controller.js
- */
 sap.ui.define(['sap/ui/core/mvc/Controller'], function (Controller) {
     'use strict';
 
     class App extends Controller {
         constructor() {
-            const fnClass = Controller.extend('general-ui.controller.App', {});
+            const fnClass = Controller.extend('manage-ui.controller.App', {});
             App.prototype.getMetadata = fnClass.prototype.getMetadata;
-            super('general-ui.controller.App');
+            super('manage-ui.controller.App');
         }
     }
 
